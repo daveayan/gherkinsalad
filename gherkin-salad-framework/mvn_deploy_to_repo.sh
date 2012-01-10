e789ad6e87b4dc170fe4340a90a4d4be58e6d046
Ayan Dave <daveayan@gmail.com>
2012-01-10 07:04:08 -0500

    mvn_deploy_to_repo now skips all tests

--- gherkin-salad-framework/mvn_deploy_to_repo.sh
+++ gherkin-salad-framework/mvn_deploy_to_repo.sh
@@ -1,1 +1,1 @@
-mvn -DaltDeploymentRepository=repo::default::file:../../_mavenrepository/releases clean deploy
+mvn -DaltDeploymentRepository=repo::default::file:../../_mavenrepository/releases clean deploy -Dmaven.test.skip=true
