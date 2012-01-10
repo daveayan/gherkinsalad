dbf5d5abeffc3400424a92d1b174c16bb68f7793
Ayan Dave <daveayan@gmail.com>
2012-01-10 14:39:41 -0500

    Revert "make component class abstract"
    This reverts commit 77d68f7bec9e25ddd3d4e6336b9ff0e6ece3cbff.

--- gherkin-salad-framework/src/main/java/daveayan/gherkinsalad/browser/actions/builtins/Component.java
+++ gherkin-salad-framework/src/main/java/daveayan/gherkinsalad/browser/actions/builtins/Component.java
@@ -1,6 +1,6 @@
 package daveayan.gherkinsalad.browser.actions.builtins;
 
-public abstract class Component extends BaseBrowserElement {
+public class Component extends BaseBrowserElement {
 	public boolean isEnabled() {
 		return true;
 	}
