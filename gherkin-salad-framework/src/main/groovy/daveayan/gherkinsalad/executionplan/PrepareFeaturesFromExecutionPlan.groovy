2bd9c1b4e5e85f8c5e10440a757f4691b0f00059
Ayan Dave <daveayan@gmail.com>
2012-01-09 22:57:11 -0500

    Handle nested folder structure for features

--- gherkin-salad-framework/src/main/groovy/daveayan/gherkinsalad/executionplan/PrepareFeaturesFromExecutionPlan.groovy
+++ gherkin-salad-framework/src/main/groovy/daveayan/gherkinsalad/executionplan/PrepareFeaturesFromExecutionPlan.groovy
@@ -5,7 +5,7 @@ import daveayan.gherkinsalad.Path
 class PrepareFeaturesFromExecutionPlan {
 	public static void main(String[] args) {
 		PrepareFeaturesFromExecutionPlan object = new PrepareFeaturesFromExecutionPlan()
-		object.run(args[0])
+		object.run("integration-test-execution-plan.csv")
 	}
 	
 	def run(file_name) {
@@ -33,21 +33,35 @@ class PrepareFeaturesFromExecutionPlan {
 		}
 	}
 	
-	def process_feature_file(feature_file_name, def top_scenario_text, bottom_scenario_text) {
+	def process_feature_file(feature_file_name, top_scenario_text, bottom_scenario_text) {
 		def feature_file = new File(Path.TO_FEATURES + feature_file_name)
 		if(feature_file.exists()) {
 			println "--> Processing Feature File '${feature_file.absolutePath}'"
 			def processed_content = feature_file.text.replace("#EXECUTION PLAN INSTRUCTION AT TOP", top_scenario_text);
 			processed_content = processed_content.replace("#EXECUTION PLAN INSTRUCTION AT BOTTOM", bottom_scenario_text);
 			
-			def new_file_name = feature_file.name.replace(".feature", "_" + System.currentTimeMillis() + ".feature");
-			
+			def file_relative_path_and_name = feature_file_name.toString()
+			def new_file_name = file_relative_path_and_name.replace(".feature", "_" + System.currentTimeMillis() + ".feature");
+
+			create_intermediate_folders (file_relative_path_and_name)
 			def processed_feature_file = new File("./features/" + new_file_name)
 			processed_feature_file.createNewFile()
 			processed_feature_file.write(processed_content);
 		} else {
 			println "--> File referred to by execution plan does not exist '${feature_file.absolutePath}'"
 		}
+	}
+	
+	def create_intermediate_folders(file_relative_path_and_name) {
+		def path_elements = file_relative_path_and_name.split("/")
+		def path_to_create = "./features/"
+		path_elements.each {
+			if(! it.endsWith(".feature")) {
+				path_to_create += it + "/"
+			}
+		}
+		def folders_to_create = new File(path_to_create)
+		folders_to_create.mkdirs()
 	}
 	
 	def top_scenario_text(feature_name, browser_name, page_structure_file_name, url, data_file_name, data_source_driver) {
