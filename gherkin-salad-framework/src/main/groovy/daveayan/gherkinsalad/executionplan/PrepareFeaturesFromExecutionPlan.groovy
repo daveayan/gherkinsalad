4d8302ab1029602afd26ef50437596b3fc2801c0
Ayan Dave <daveayan@gmail.com>
2012-03-11 23:16:38 -0400

    Execution File can now be a xls file or a csv file

--- gherkin-salad-framework/src/main/groovy/daveayan/gherkinsalad/executionplan/PrepareFeaturesFromExecutionPlan.groovy
+++ gherkin-salad-framework/src/main/groovy/daveayan/gherkinsalad/executionplan/PrepareFeaturesFromExecutionPlan.groovy
@@ -5,6 +5,7 @@ import org.apache.commons.logging.LogFactory
 
 import daveayan.gherkinsalad.Config
 import daveayan.gherkinsalad.Path
+import daveayan.gherkinsalad.datamanagement.XlsFileSupport
 
 class PrepareFeaturesFromExecutionPlan {
 	private static Log log = LogFactory.getLog(PrepareFeaturesFromExecutionPlan.class)
@@ -15,34 +16,75 @@ class PrepareFeaturesFromExecutionPlan {
 	}
 	
 	def run(file_name) {
+		if(file_name.endsWith(".csv")) {
+			process_execution_plan_csv(file_name)
+		} else {
+			process_execution_plan_xls(file_name)
+		}
+	}
+	
+	def process_execution_plan_xls(file_name) {
+		String[] file_name_parts = file_name.split("-in-")
 		println """
 #########################################################
-# GHERKIN SALAD 
-# Applying execution plan 
+# GHERKIN SALAD
+# Applying execution plan
+# ${file_name_parts[0]} in ${file_name_parts[1]}
+#########################################################
+"""
+		XlsFileSupport support = XlsFileSupport.openXlsFile(Path.TO_EXECUTION_PLAN + file_name_parts[0])
+		if(support.file_exists()) {
+			support.switch_to_sheet(file_name_parts[1])
+			if(support.sheet_exists()) {
+				int row_number = 0;
+				while(true) {
+					String feature_file_name 		= support.value_in_cell("A_" + row_number)
+					String browser 							= support.value_in_cell("B_" + row_number)
+					String page_structure 			= support.value_in_cell("C_" + row_number)
+					String env 									= support.value_in_cell("D_" + row_number)
+					String data_file 						= support.value_in_cell("E_" + row_number)
+					if(feature_file_name.contains("QUEUE UP")) { 
+						break
+					}
+					apply_execution_plan_line_item(feature_file_name, "default", browser, page_structure, env, data_file, "DEFAULT")
+					row_number ++
+				}
+			} else {
+			println "Skipping file ${file_name_parts[0]} in environment ${file_name_parts[1]} as the sheet does not exist"
+			}
+		} else {
+			println "Skipping file ${file_name_parts[0]} as it does not exist"
+		}
+	}
+	
+	def process_execution_plan_csv(file_name) {
+		println """
+#########################################################
+# GHERKIN SALAD
+# Applying execution plan
 # ${file_name}
 #########################################################
 """
-		process_execution_plan(file_name)
-	}
-	
-	def process_execution_plan(file_name) {
 		def execution_plan_file = new File(Path.TO_EXECUTION_PLAN + file_name)
 		if(execution_plan_file.exists()) {
 			execution_plan_file.eachLine { line ->
 				if(! line.startsWith("feature file")) {
 					def line_items = line.split(",")
-					def browser = line_items[2];
-					if(allow_browser_execution(browser)) {
-						def top_scenario_text = top_scenario_text(line_items[1], line_items[2], line_items[3], line_items[4], line_items[5], line_items[6])
-						def bottom_scenario_text = bottom_scenario_text()
-						process_feature_file(line_items[0], top_scenario_text, bottom_scenario_text)
-					} else {
-						println "Skipping ${browser} execution of feature ${line_items[0]}"
-					}
+					apply_execution_plan_line_item(line_items[0], line_items[1], line_items[2], line_items[3], line_items[4], line_items[5], line_items[6])
 				}
 			}
 		} else {
 			println "Skipping file ${execution_plan_file} as it does not exist"
+		}
+	}
+	
+	def apply_execution_plan_line_item(feature_file, feature_name, browser_name, page_structure_file_name, env_name, data_file_name, data_source_driver) {
+		if(allow_browser_execution(browser_name)) {
+			def top_scenario_text = top_scenario_text(feature_name, browser_name, page_structure_file_name, env_name, data_file_name, data_source_driver)
+			def bottom_scenario_text = bottom_scenario_text()
+			process_feature_file(feature_file, top_scenario_text, bottom_scenario_text)
+		} else {
+			println "Skipping ${browser_name} execution of feature ${feature_file}"
 		}
 	}
 	
