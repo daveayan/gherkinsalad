f4254a6cde92c5f70f7ef6ac11b694c91bed5b4c
Ayan Dave <daveayan@gmail.com>
2012-07-30 08:20:31 -0400

    Reporting updated

--- src/main/java/daveayan/gherkinsalad/report/Report.java
+++ src/main/java/daveayan/gherkinsalad/report/Report.java
@@ -7,19 +7,30 @@ import java.util.Date;
 
 import org.apache.commons.io.FileUtils;
 
+import daveayan.gherkinsalad.Path;
+
 public class Report {
+	
+	public static void goal(String goal) {
+		report(goal);
+	}
+	
+	public static void task(String task) {
+		report("- " + task);
+	}
+	
 	public static void action(String action) {
-		report(action);
+		report("-- " + action);
 	}
 	
 	public static void screenshot_taken(String filename) {
-		report("Screen shot taken at '" + filename + "'");
+		action("Screen shot taken at '" + filename + "'");
 	}
 	
 	private static void report(String text) {
 		DateFormat df = DateFormat.getDateTimeInstance();
 		String datetime = df.format(new Date());
-		File report_file = new File("report.txt");
+		File report_file = new File(Path.TO_TARGET + "report.txt");
 		try {
 			FileUtils.writeStringToFile(report_file, "\n" + datetime + " : " + text, true);
 		} catch (IOException e) {
