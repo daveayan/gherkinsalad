89a581044125fb04116551bc7e92123ec5bde9b9
Fadi Abdin <fadiabdeen@gmail.com>
2012-10-11 08:23:40 -0400

    I guess we dont need that anymore

--- gherkinsalad/execute-plans.sh
+++ gherkinsalad/execute-plans.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 echo "GHERKIN SALAD"
 echo mvn clean integration-test -Dapply-execution-plan.planfiles=$1
-mvn clean integration-test -Dapply-execution-plan.planfiles=$1
+#mvn clean integration-test -Dapply-execution-plan.planfiles=$1
 
 echo mvn daveayan:gherkin-salad-mojo:1.0:archive-execution-results
-mvn daveayan:gherkin-salad-mojo:1.0:archive-execution-results
+#mvn daveayan:gherkin-salad-mojo:1.0:archive-execution-results
