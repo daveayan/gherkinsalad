9f07aef7cdb5534eab4a3466a032ba6a1eab756e
Ayan Dave <daveayan@gmail.com>
2013-02-09 18:07:40 -0500

    fixed broken unit test

--- gherkinsalad.jquery/src/test/java/daveayan/gherkinsalad/ReportStreamingTest.java
+++ gherkinsalad.jquery/src/test/java/daveayan/gherkinsalad/ReportStreamingTest.java
@@ -8,7 +8,7 @@ import org.openqa.selenium.io.IOUtils;
 
 public class ReportStreamingTest {
 	@Test public void stream_from_core_jar_should_be_readable() throws IOException {
-		InputStream stream = this.getClass().getResourceAsStream("/gherkinsaladreport.js");
+		InputStream stream = this.getClass().getResourceAsStream("/defaultreport/gherkinsaladreport.js");
 		String string = IOUtils.readFully(stream);
 		System.out.println(string);
 	}
